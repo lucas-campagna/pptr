c4618ff521c26d2d5558d7633753819cf0a980be
Lucas Campagna <lucas.campagna@example.com>
2026-03-18 21:07:12 -0300

    fix: bundle Chrome and libs with executable for standalone use

--- src/runner.js
+++ src/runner.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const path = require('path');
+const fs = require('fs');
 const Logger = require('./logger');
 const Parser = require('./parser');
 const VariableEngine = require('./variables');
@@ -16,6 +18,64 @@ class Runner {
     };
   }
 
+  findBundledChrome() {
+    const exePath = process.execPath;
+    const exeDir = path.dirname(exePath);
+    
+    const chromePaths = [
+      path.join(exeDir, 'chrome', 'chrome'),
+      path.join(exeDir, '..', 'chrome', 'chrome'),
+      path.join(exeDir, 'chrome', 'chrome-linux64', 'chrome'),
+      path.join(exeDir, 'pptr', 'chrome', 'chrome'),
+    ];
+
+    for (const chromePath of chromePaths) {
+      if (fs.existsSync(chromePath)) {
+        return chromePath;
+      }
+    }
+
+    const possibleDirs = [
+      exeDir,
+      path.join(exeDir, '..'),
+      path.join(exeDir, 'dist'),
+      path.join(exeDir, 'release'),
+    ];
+
+    for (const dir of possibleDirs) {
+      const chromeBin = path.join(dir, 'chrome', 'chrome');
+      if (fs.existsSync(chromeBin)) {
+        return chromeBin;
+      }
+      
+      const chromeLinuxDir = path.join(dir, 'chrome', 'chrome-linux64');
+      if (fs.existsSync(chromeLinuxDir)) {
+        const chrome = path.join(chromeLinuxDir, 'chrome');
+        if (fs.existsSync(chrome)) {
+          return chrome;
+        }
+      }
+    }
+
+    return null;
+  }
+
+  setupChromeEnvironment() {
+    const chromePath = this.findBundledChrome();
+    if (chromePath) {
+      const chromeDir = path.dirname(chromePath);
+      process.env.PUPPETEER_EXECUTABLE_PATH = chromePath;
+      process.env.PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = 'true';
+      if (process.env.LD_LIBRARY_PATH) {
+        process.env.LD_LIBRARY_PATH = `${chromeDir}:${process.env.LD_LIBRARY_PATH}`;
+      } else {
+        process.env.LD_LIBRARY_PATH = chromeDir;
+      }
+      return chromePath;
+    }
+    return null;
+  }
+
   async run(scriptPath) {
     const parser = new Parser();
     const script = parser.parseFile(scriptPath);
@@ -31,11 +91,19 @@ class Runner {
       this.options.headless = false;
     }
 
-    const browser = await puppeteer.launch({
+    const launchOptions = {
       headless: this.options.headless,
       slowMo: meta.slowMo || this.options.slowMo,
       args: browserArgs,
-    });
+    };
+
+    const bundledChrome = this.setupChromeEnvironment();
+    if (bundledChrome) {
+      logger.info(`Using bundled Chrome: ${bundledChrome}`);
+      launchOptions.executablePath = bundledChrome;
+    }
+
+    const browser = await puppeteer.launch(launchOptions);
 
     let result;
     try {
@@ -88,11 +156,19 @@ class Runner {
 
     const browserArgs = ['--no-sandbox', '--disable-setuid-sandbox'];
 
-    const browser = await puppeteer.launch({
+    const launchOptions = {
       headless: this.options.headless,
       slowMo: meta.slowMo || this.options.slowMo,
       args: browserArgs,
-    });
+    };
+
+    const bundledChrome = this.setupChromeEnvironment();
+    if (bundledChrome) {
+      logger.info(`Using bundled Chrome: ${bundledChrome}`);
+      launchOptions.executablePath = bundledChrome;
+    }
+
+    const browser = await puppeteer.launch(launchOptions);
 
     let result;
     try {
