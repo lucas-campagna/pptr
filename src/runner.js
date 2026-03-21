c467147afc123efa7f76c2f5c1e84ca79e5f7c2b
Lucas Prett Campagna <lucasprett129@gmail.com>
2026-03-21 15:27:29 -0300

    Style

--- src/runner.js
+++ src/runner.js
@@ -1,11 +1,11 @@
-const puppeteer = require('puppeteer');
-const path = require('path');
-const fs = require('fs');
-const { spawn } = require('child_process');
-const Logger = require('./logger');
-const Parser = require('./parser');
-const VariableEngine = require('./variables');
-const Interpreter = require('./interpreter');
+const puppeteer = require("puppeteer");
+const path = require("path");
+const fs = require("fs");
+const { spawn } = require("child_process");
+const Logger = require("./logger");
+const Parser = require("./parser");
+const VariableEngine = require("./variables");
+const Interpreter = require("./interpreter");
 
 class Runner {
   constructor(options = {}) {
@@ -16,14 +16,14 @@ class Runner {
       logPath: options.logPath,
       debug: options.debug || false,
       vars: options.vars || {},
-      version: options.version || '1.0.0',
+      version: options.version || "1.0.0",
       subcommands: options.subcommands || [],
       ...options,
     };
   }
 
   getVersion() {
-    return this.options.version || '1.0.0';
+    return this.options.version || "1.0.0";
   }
 
   getDepsDir() {
@@ -31,7 +31,7 @@ class Runner {
   }
 
   getChromeDir() {
-    return path.join(this.getDepsDir(), 'chrome');
+    return path.join(this.getDepsDir(), "chrome");
   }
 
   async ensureDependencies(logger) {
@@ -47,48 +47,52 @@ class Runner {
 
     fs.mkdirSync(depsDir, { recursive: true });
 
-    const tarball = path.join(depsDir, 'deps.tar.gz');
+    const tarball = path.join(depsDir, "deps.tar.gz");
     const url = `https://github.com/lucas-campagna/pptr/releases/download/base-deps/deps.tar.gz`;
 
     await this.downloadWithProgress(url, tarball, logger);
 
-    logger.debug('Extracting dependencies...');
+    logger.debug("Extracting dependencies...");
     await this.extractTarGz(tarball, depsDir, logger);
 
     try {
       fs.unlinkSync(tarball);
     } catch (e) {}
 
-    logger.debug('Dependencies ready');
+    logger.debug("Dependencies ready");
     return chromeDir;
   }
 
   async downloadWithProgress(url, dest, logger) {
-    const https = require('https');
-    const http = require('http');
-    const { URL } = require('url');
+    const https = require("https");
+    const http = require("http");
+    const { URL } = require("url");
 
     return new Promise((resolve, reject) => {
       const urlObj = new URL(url);
-      const client = urlObj.protocol === 'https:' ? https : http;
+      const client = urlObj.protocol === "https:" ? https : http;
 
       const request = client.get(urlObj, (response) => {
         if (response.statusCode === 302 || response.statusCode === 301) {
           const redirectUrl = response.headers.location;
           logger.debug(`Following redirect to ${redirectUrl}`);
-          return this.downloadWithProgress(redirectUrl, dest, logger).then(resolve).catch(reject);
+          return this.downloadWithProgress(redirectUrl, dest, logger)
+            .then(resolve)
+            .catch(reject);
         }
 
         if (response.statusCode !== 200) {
-          return reject(new Error(`Failed to download: ${response.statusCode}`));
+          return reject(
+            new Error(`Failed to download: ${response.statusCode}`),
+          );
         }
 
-        const total = parseInt(response.headers['content-length'], 10);
+        const total = parseInt(response.headers["content-length"], 10);
         let downloaded = 0;
 
         const file = fs.createWriteStream(dest);
 
-        response.on('data', (chunk) => {
+        response.on("data", (chunk) => {
           downloaded += chunk.length;
           if (total > 0) {
             const percent = ((downloaded / total) * 100).toFixed(1);
@@ -96,41 +100,41 @@ class Runner {
           }
         });
 
-        file.on('finish', () => {
-          process.stdout.write('\n');
+        file.on("finish", () => {
+          process.stdout.write("\n");
           resolve();
         });
 
-        file.on('error', reject);
-        response.on('error', reject);
+        file.on("error", reject);
+        response.on("error", reject);
 
         response.pipe(file);
       });
 
-      request.on('error', reject);
+      request.on("error", reject);
     });
   }
 
   async extractTarGz(tarball, dest) {
     return new Promise((resolve, reject) => {
-      const tar = spawn('tar', ['-xzf', tarball, '-C', dest]);
-      tar.on('close', (code) => {
+      const tar = spawn("tar", ["-xzf", tarball, "-C", dest]);
+      tar.on("close", (code) => {
         if (code === 0) resolve();
         else reject(new Error(`tar exited with code ${code}`));
       });
-      tar.on('error', reject);
+      tar.on("error", reject);
     });
   }
 
   createVariableEngine(script) {
     const declaredVars = [];
-    
+
     function collectVars(obj) {
       if (!obj) return;
-      if (obj.vars && typeof obj.vars === 'object') {
+      if (obj.vars && typeof obj.vars === "object") {
         declaredVars.push(...Object.keys(obj.vars));
       }
-      if (obj.subcommands && typeof obj.subcommands === 'object') {
+      if (obj.subcommands && typeof obj.subcommands === "object") {
         for (const sub of Object.values(obj.subcommands)) {
           collectVars(sub);
         }
@@ -144,7 +148,7 @@ class Runner {
     }
 
     const vars = new VariableEngine({}, declaredVars);
-    vars.set('env', process.env);
+    vars.set("env", process.env);
 
     for (const [key, value] of Object.entries(process.env)) {
       if (key.match(/^[A-Z][A-Z0-9_]*$/)) {
@@ -157,10 +161,10 @@ class Runner {
     function setVars(obj) {
       if (!obj || !obj.vars) return;
       Object.entries(obj.vars).forEach(([key, value]) => {
-        if (typeof value === 'string' && value.startsWith('${env.')) {
+        if (typeof value === "string" && value.startsWith("${env.")) {
           const envVar = value.match(/\$\{env\.([^}]+)\}/)?.[1];
           if (envVar) {
-            vars.set(key, process.env[envVar] || '');
+            vars.set(key, process.env[envVar] || "");
           }
         } else if (!vars.vars[key]) {
           vars.set(key, value);
@@ -198,32 +202,33 @@ class Runner {
     const logPath = this.options.logPath || meta.logs;
 
     const logger = new Logger(logPath, this.options.debug);
-    logger.debug('Initializing Puppeteer');
+    logger.debug("Initializing Puppeteer");
 
     const chromeDir = await this.ensureDependencies(logger);
 
     const browserArgs = [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--no-zygote',
-      '--disable-gpu',
-      '--ignore-certificate-errors',
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--no-zygote",
+      "--disable-gpu",
+      "--ignore-certificate-errors",
     ];
 
     if (meta.headless === false) {
       this.options.headless = false;
     } else {
-      browserArgs.push('--headless=new');
+      browserArgs.push("--headless=new");
     }
 
-    const { HTTP_PROXY, HTTPS_PROXY, http_proxy, https_proxy, ...restEnv } = process.env;
+    const { HTTP_PROXY, HTTPS_PROXY, http_proxy, https_proxy, ...restEnv } =
+      process.env;
 
     const launchOptions = {
       headless: this.options.headless,
       slowMo: meta.slowMo || this.options.slowMo,
       args: browserArgs,
-      executablePath: path.join(chromeDir, 'chrome'),
+      executablePath: path.join(chromeDir, "chrome"),
       env: {
         ...restEnv,
         LD_LIBRARY_PATH: path.join(chromeDir, "lib"),
@@ -247,7 +252,7 @@ class Runner {
       result = await interpreter.run(script);
     } finally {
       await browser.close();
-      logger.debug('Browser closed');
+      logger.debug("Browser closed");
     }
 
     return result;
@@ -261,32 +266,33 @@ class Runner {
     const logPath = this.options.logPath || meta.logs;
 
     const logger = new Logger(logPath, this.options.debug);
-    logger.debug('Initializing Puppeteer');
+    logger.debug("Initializing Puppeteer");
 
     const chromeDir = await this.ensureDependencies(logger);
 
     const browserArgs = [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--no-zygote',
-      '--disable-gpu',
-      '--ignore-certificate-errors',
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--no-zygote",
+      "--disable-gpu",
+      "--ignore-certificate-errors",
     ];
 
     if (meta.headless === false) {
       this.options.headless = false;
     } else {
-      browserArgs.push('--headless=new');
+      browserArgs.push("--headless=new");
     }
 
-    const { HTTP_PROXY, HTTPS_PROXY, http_proxy, https_proxy, ...restEnv } = process.env;
+    const { HTTP_PROXY, HTTPS_PROXY, http_proxy, https_proxy, ...restEnv } =
+      process.env;
 
     const launchOptions = {
       headless: this.options.headless,
       slowMo: meta.slowMo || this.options.slowMo,
       args: browserArgs,
-      executablePath: path.join(chromeDir, 'chrome'),
+      executablePath: path.join(chromeDir, "chrome"),
       env: {
         ...restEnv,
         LD_LIBRARY_PATH: path.join(chromeDir, "lib"),
@@ -310,7 +316,7 @@ class Runner {
       result = await interpreter.run(script);
     } finally {
       await browser.close();
-      logger.debug('Browser closed');
+      logger.debug("Browser closed");
     }
 
     return result;
