95d20f450259ba116044272ec3dad78a17e42e13
Lucas Prett Campagna <lucasprett129@gmail.com>
2026-05-14 19:00:10 -0300

    chore: remove old package folders and outdated package.jsons

--- apps/pptr/src/index.js
+++
@@ -1,8 +0,0 @@
-// Provide compatibility for imports that require the old package name.
-// Prefer the merged in-repo implementation at ../../src/libs, otherwise fall
-// back to an installed pptr-core package if present.
-try {
-  module.exports = require('../../src/libs');
-} catch (e) {
-  module.exports = require('pptr-core');
-}
---
+++ src/index.js
@@ -0,0 +1,3 @@
+// Main entrypoint for the flattened package.
+// Re-export the in-repo libs implementation.
+module.exports = require('./libs');
